a63cecbcab840f2e1a525f087df91f698a39b6fd
evnag <vskonev22@gmail.com>
2023-11-29 19:47:04 +0300

    Initial commit

---
+++ src/main/java/com/bullseye/tracker/BullseyeTaskTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.bullseye.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BullseyeTaskTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BullseyeTaskTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bullseye/tracker/BullseyeTaskTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bullseye.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BullseyeTaskTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
