7aa84dc6db27c3bedae374c2ae4b9efb1886f36c
evnag <vskonev22@gmail.com>
2023-12-04 19:58:15 +0300

    Add api for User with some endpoints

---
+++ src/main/java/com/bullseye/tracker/service/UserService.java
@@ -0,0 +1,26 @@
+package com.bullseye.tracker.service;
+
+import com.bullseye.tracker.model.User;
+import com.bullseye.tracker.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository repository;
+
+    public User save(User user) {
+        if (user == null) {
+            return null;
+        }
+        return repository.save(user);
+    }
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+}
